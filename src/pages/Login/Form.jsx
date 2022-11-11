711200ed57f09fe23a92c4808c006361fbcd13e6
marcelosrc <marcelosrc@gmail.com>
2022-11-11 19:53:13 -0300

    Conclusão das funções estéticas na página de login

--- src/pages/Login/Form.jsx
+++ src/pages/Login/Form.jsx
@@ -1,12 +1,23 @@
+function greetings() {
+    const date = new Date()
+    if (date.getHours() >= 19) {
+        return "Boa noite"
+    } else if (date.getHours() >= 12  && date.getHours() < 19) {
+        return "Boa tarde"
+    } else {
+        return "Bom dia"
+    }
+}
+
 export default function Form() {
     return (
         <form className="rightside-pane fadein">
-            <h2>greetings()</h2><br/>
+            <h2>{greetings()}</h2><br/>
             <p>Email</p>
             <input className="standard-input" id="campo-user" name="email" type="text" aria-label="usuário"/><br/>
             <p>Senha</p>
             <input className="standard-input" id="campo-password" name="password" type="password" aria-label="senha"/>
-            <a href="https://giphy.com/gifs/se-fudeu-fodeu-Y4c8GgvNh7BiBbv8fp" target="_blank">
+            <a href="https://giphy.com/gifs/se-fudeu-fodeu-Y4c8GgvNh7BiBbv8fp" target="_blank" rel="noreferrer">
                 <p>Esqueci minha senha</p>
             </a>
             <a href="/register">
--- src/pages/Login/PicturePane.jsx
+++ src/pages/Login/PicturePane.jsx
@@ -7,8 +7,7 @@ import pic4 from '../../images/home-rnd-pics/877.jpg'
 export default function PicturePane() {
     const randomPics = [pic0, pic1, pic2, pic3, pic4]
     const randomNum = Math.floor(Math.random() * 5)
-    console.log(randomNum)
     return (
-        <img className="tint leftside" src={randomPics[randomNum]} width="400" height="400" />
+        <img className="tint leftside" src={randomPics[randomNum]} width="400" height="400" alt="social" />
     )
 }
