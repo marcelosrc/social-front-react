a87cb647ce01138dc0d56c5dee5946739b2c3121
marcelosrc <marcelosrc@gmail.com>
2022-12-15 22:27:45 -0300

    Implantado React.lazy em profile e feed (any e user)

--- src/pages/AnyUser/AnyUserPage.jsx
+++ src/pages/AnyUser/AnyUserPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import LoadingPage from "../../components/LoadingPage";
 import CardsPanel from "../../components/CardsPanel/CardsPanel";
 
 const AnyUserProfile = React.lazy(() => import("../AnyUser/AnyUserProfile"));
@@ -27,8 +28,24 @@ export default function UserPage() {
 
   return (
     <div className="homepage-flex-container">
-      <AnyUserProfile anyUser={anyUser} setReloadAnyUser={setReloadAnyUser} />
-      <AnyUserFeed anyUser={anyUser} />
+      <React.Suspense
+        fallback={
+          <div className="profile">
+            <LoadingPage />
+          </div>
+        }
+      >
+        <AnyUserProfile anyUser={anyUser} setReloadAnyUser={setReloadAnyUser} />
+      </React.Suspense>
+      <React.Suspense
+        fallback={
+          <div className="generalfeed">
+            <LoadingPage />
+          </div>
+        }
+      >
+        <AnyUserFeed anyUser={anyUser} />
+      </React.Suspense>
       <CardsPanel anyUser={anyUser} />
     </div>
   );
--- src/pages/User/UserPage.jsx
+++ src/pages/User/UserPage.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import UserProfile from "./UserProfile";
-import UserFeed from "./UserFeed";
+import LoadingPage from "../../components/LoadingPage";
 import CardsPanel from "../../components/CardsPanel/CardsPanel";
+
+const UserProfile = React.lazy(() => import("./UserProfile"));
+const UserFeed = React.lazy(() => import("./UserFeed"));
 
 export default function UserPage() {
   return (
     <div className="homepage-flex-container">
-      <UserProfile />
-      <UserFeed />
+      <React.Suspense
+        fallback={
+          <div className="profile">
+            <LoadingPage />
+          </div>
+        }
+      >
+        <UserProfile />
+      </React.Suspense>
+      <React.Suspense
+        fallback={
+          <div className="generalfeed">
+            <LoadingPage />
+          </div>
+        }
+      >
+        <UserFeed />
+      </React.Suspense>
       <CardsPanel />
     </div>
   );
